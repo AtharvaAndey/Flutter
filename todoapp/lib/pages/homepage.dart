17415688f94f0e60dc0f49885f6457416df5e433
Atharva Andey <atharvaandey11@gmail.com>
2024-03-13 06:50:40 +0530

    modification: UI  - color change

--- todoapp/lib/pages/homepage.dart
+++ todoapp/lib/pages/homepage.dart
@@ -81,7 +81,7 @@ class _HomeState extends State<Home> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.blue,
-        fixedColor: Colors.blue,
+        fixedColor: Colors.red,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.people),
